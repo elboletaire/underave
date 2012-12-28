3f69384306377c602a461086ec646a3e2adfc24f
Òscar Casajuana <elboletaire@gmail.com>
2012-12-28 19:51:01 +0100

    Added awstats config creation

--- vhostmanager.sh
+++ vhostmanager.sh
@@ -117,18 +117,14 @@ init () {
 	case $action in
 
 		"add")
-			verbose "+ Creating user.."
 			create_user
-			verbose "++ Creating folders & data.."
 			create_data
-			verbose "+++ Creating virtual host.."
 			create_virtual_host
-			verbose "++++ Creating htpasswd file"
 			htpasswd_add
-			verbose "+++++ Setting permissions"
+			create_awstats_file
 			set_permissions
-			verbose "++++++ Reloading daemons"
 			reload_daemons
+			create_cron
 		;;
 
 		"remove")
@@ -139,13 +135,9 @@ init () {
 				read yesno
 			done
 			if [ $yesno = "yes" ]; then
-				verbose "+ Removing user.."
 				remove_user
-				verbose "++ Removing folders and contents.."
 				remove_data
-				verbose "+++ Removing virtual host.."
 				remove_virtualhost
-				verbose "++++ Reloading daemons"
 				reload_daemons
 			else
 				verbose "You're rethinking it, huh? Well done"
@@ -163,13 +155,39 @@ create_data () {
 		return
 	fi
 	# folders and aliases..
-	verbose "Creating folders and default content.."
-	mkdir -m 1750 -p $homedir/www
+	create_folders
+	verbose "Symlinking www to public_html..."
 	ln -s $homedir/www $homedir/public_html
 	# "it works" file
+	verbose "Creating index.html file..."
 	echo '<!DOCTYPE html><html><body><h1>It works, bitches</h1></body></html>' > $homedir/www/index.html
-	verbose "Creating mysql schema.."
-	mysql -e "CREATE SCHEMA $username;"
+	verbose "Creating mysql schema..."
+	mysql -e "CREATE SCHEMA IF NOT EXISTS $username;"
+}
+
+create_folders () {
+	verbose "Creating folders..."
+	mkdir -m 1750 -p $homedir/www
+	mkdir -m 1750 -p $homedir/tmp/awstats/conf
+	mkdir -m 1750 -p $homedir/tmp/awstats/lib
+	mkdir -m 1750 -p $homedir/tmp/log
+}
+
+create_awstats_file () {
+	verbose "Creating awstats configuration"
+	echo "# BASIC AWSTATS CONFIGURATION
+Include \"/etc/awstats/awstats.conf.local\"
+
+LogFile=\"$homedir/tmp/log/$username-access.log\"
+
+SiteDomain=\"www.$fqdn\"
+
+# Example: \"www.myserver.com localhost 127.0.0.1 REGEX[mydomain\.(net|org)\$]\"
+HostAliases=\"localhost 127.0.0.1 $fqdn www.$fqdn\"
+
+DirData=\"$homedir/tmp/awstats/lib/\"
+
+AllowAccessFromWebToFollowingAuthenticatedUsers=\"elboletaire $username\"" > $homedir/tmp/awstats/conf/awstats.$fqdn.conf
 }
 
 create_user () {
@@ -177,12 +195,12 @@ create_user () {
 		verbose "User $username exists, skipping.."
 		return
 	fi
-	verbose "Creating system user.."
+	verbose "Creating system user..."
 	useradd $username -p $password -d $homedir -s /bin/bash
-	verbose "Creating mysql user.."
+	verbose "Creating mysql user..."
 	mysql -e "GRANT ALL PRIVILEGES ON $username.* TO $username@'localhost' IDENTIFIED BY '$password';"
-	mysql -e "GRANT ALL PRIVILEGES ON $username.* TO $username@'ns1.wonky.es' IDENTIFIED BY '$password';"
-	mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE ON phpmyadmin.* TO $username@'ns1.wonky.es';"
+	mysql -e "GRANT ALL PRIVILEGES ON $username.* TO $username@'192.168.0.6' IDENTIFIED BY '$password';"
+	mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE, LOCK TABLES ON phpmyadmin.* TO $username@'192.168.0.6';"
 }
 
 check_user_exists () {
@@ -195,7 +213,7 @@ remove_user () {
 	[ $force ] && userdel -f $username || userdel $username
 	verbose "Removing mysql user.."
 	mysql -e "DROP USER $username@'localhost';"
-	mysql -e "DROP USER $username@'ns1.wonky.es';"
+	mysql -e "DROP USER $username@'192.168.0.6';"
 }
 
 remove_data () {
@@ -205,64 +223,79 @@ remove_data () {
 	if [ ! $force ]; then
 		[ $verbose ] && rm -v $homedir || rm $homedir
 	else
-		[ $verbose ] && rm -rv $homedir || rm -r $homedir
+		[ $verbose ] && rm -rv $homedir || rm -fr $homedir
 	fi
 }
 
 # set permissions
 set_permissions () {
+	verbose "Setting user permissions..."
 	chown -R $username:$usergroup $homedir
 	chmod -R 1775 $homedir/www
 }
 
 create_virtual_host () {
-	echo "<VirtualHost *:80>
-		ServerAdmin elboletaire@underave.net
-		ServerName $fqdn
-		ServerAlias www.$fqdn
+	verbose "Creating apache virtual host..."
+	echo "
+<VirtualHost *:80>
+	ServerAdmin elboletaire@underave.net
+	ServerName panel.$fqdn
 
-		DocumentRoot $homedir/www/
-		<Directory />
-			Options FollowSymLinks
-			AllowOverride none
-		</Directory>
-		<Directory $homedir/www/>
-			Options Indexes FollowSymLinks MultiViews
-			AllowOverride All
-			Order allow,deny
-			Allow from all
+	DocumentRoot /var/www/panel/
 
-			RewriteEngine On
+	<Directory /var/www/panel/>
+		Options FollowSymLinks
+		AllowOverride all
 
-			RewriteCond %{HTTP_HOST} ^(www\.)?(.+)$ [NC]
-			RewriteRule ^stats\/?$ /awstats/awstats.pl?config=%2 [R=301,L]
-		</Directory>
+		RewriteEngine On
+		RewriteRule ^stats\/?$ /awstats/awstats.pl?configdir=$homedir/tmp/awstats/conf/&config=$fqdn [R=301,L]
+	</Directory>
 
-		<Location /awstats>
-			AuthType Basic
-			AuthName \"awstats\"
-			AuthUserFile $homedir/.htpasswd
-			Require valid-user
-		</Location>
+	 <Location />
+		AuthType Basic
+		AuthName \"$fqdn control panel\"
+		AuthUserFile $homedir/.htpasswd
+		Require valid-user
+		SetEnv AWSTATS_ENABLE_CONFIG_DIR 1
+	</Location>
+</VirtualHost>
+<VirtualHost *:80>
+	ServerAdmin elboletaire@underave.net
+	ServerName $fqdn
+	ServerAlias www.$fqdn
 
-		ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
-		<Directory \"/usr/lib/cgi-bin\">
-			AllowOverride None
-			Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
-			Order allow,deny
-			Allow from all
-		</Directory>
+	DocumentRoot $homedir/www/
 
-		ErrorLog ${APACHE_LOG_DIR}/$username-error.log
+	<Directory $homedir/www/>
+		Options Indexes FollowSymLinks MultiViews
+		AllowOverride All
+		Order allow,deny
+		Allow from all
+	</Directory>
 
-		# Possible values include: debug, info, notice, warn, error, crit,
-		# alert, emerg.
-		LogLevel warn
+	ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
+	<Directory \"/usr/lib/cgi-bin\">
+		AllowOverride None
+		Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
+		Order allow,deny
+		Allow from all
+	</Directory>
 
-		CustomLog ${APACHE_LOG_DIR}/$username-access.log combined
+	ErrorLog $homedir/tmp/log/$username-error.log
+
+	# Possible values include: debug, info, notice, warn, error, crit,
+	# alert, emerg.
+	LogLevel warn
+
+	CustomLog $homedir/tmp/log/$username-access.log combined
 </VirtualHost>" > /etc/apache2/sites-available/$fqdn
 
 	a2ensite $fqdn
+}
+
+create_cron () {
+	verbose "Creating cron..."
+	echo "*/30 * * * * /usr/local/bin/awstats_updateall.pl -configdir=$homedir/tmp/awstats/conf/ -awstatsprog=/usr/lib/cgi-bin/awstats.pl now" | crontab -u $username -
 }
 
 remove_virtualhost () {
